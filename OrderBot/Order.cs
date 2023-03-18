5ccdd4bb9820e310a1784ab2a3842376341b8d9a
Preetb71 <preetbhatt.p@gmail.com>
2023-03-18 16:31:32 -0400

    The PC Assembler bot working now.

--- OrderBot/Order.cs
+++ OrderBot/Order.cs
@@ -4,6 +4,7 @@ namespace OrderBot
 {
     public class Order : ISQLModel
     {
+        private string _tokenNumber = String.Empty;
         private string _size = String.Empty;
         private string _phone = String.Empty;
         private string _motherboard = String.Empty;
@@ -14,6 +15,13 @@ namespace OrderBot
         private string _cpuCase = String.Empty;
         private string _os = String.Empty;
         private string _monitor = String.Empty;
+
+        private string _confirmOrder = String.Empty;
+
+        public String TokenNumber{
+            get => _tokenNumber;
+            set => _tokenNumber = value;
+        }
 
         public string Motherboard{
             get => _motherboard;
@@ -54,7 +62,16 @@ namespace OrderBot
             set => _monitor = value;
         }
 
-        public void Save(){
+        public string ConfirmOrder{
+            get => _confirmOrder;
+            set => _confirmOrder = value;
+        }
+
+        public void Save()
+        {
+            //PREET - I HAVE CREATED A NEW TABLE INSIDE THE ORDERS.DB DATABASE CONTAINING ALL OF THE PC INFORMATION WHICH WILL GET INSERTED AND UPDATED AS
+            //THE USER WILL GIVE THEIR RESPONSES. IT WAS EASIER FOR ME TO JUST CREATE A NEW DATABASE THAN TO REMOVE THE EXISTING DATA FROM THE ORDERS TABLE.
+            //HERE I HAVE USED TOKEN NUMBER AS THE PRIMARY KEY AND IDENTIFIER FOR THE ORDER.
            using (var connection = new SqliteConnection(DB.GetConnectionString()))
             {
                 connection.Open();
@@ -62,24 +79,41 @@ namespace OrderBot
                 var commandUpdate = connection.CreateCommand();
                 commandUpdate.CommandText =
                 @"
-        UPDATE orders
-        SET size = $size
-        WHERE phone = $phone
-    ";
-                // commandUpdate.Parameters.AddWithValue("$size", Size);
-                // commandUpdate.Parameters.AddWithValue("$phone", Phone);
+                UPDATE pcOrders
+                SET motherboard = $motherboard, processor = $processor, ram = $ram, storageSSD = $storageSSD,
+                graphicsCard = $graphicsCard, cpuCase = $cpuCase, os = $os, monitor = $monitor, isOrderConfirmed = $isOrderConfirmed
+                WHERE tokenId = $_tokenNumber";
+                
+                commandUpdate.Parameters.AddWithValue("$_tokenNumber", TokenNumber);
+                commandUpdate.Parameters.AddWithValue("$motherboard", Motherboard);
+                commandUpdate.Parameters.AddWithValue("$processor", Processor);
+                commandUpdate.Parameters.AddWithValue("$ram", RAM);
+                commandUpdate.Parameters.AddWithValue("$storageSSD", Storage);
+                commandUpdate.Parameters.AddWithValue("$graphicsCard", Graphics);
+                commandUpdate.Parameters.AddWithValue("$cpuCase", CPU_Case);
+                commandUpdate.Parameters.AddWithValue("$os", OS);
+                commandUpdate.Parameters.AddWithValue("$monitor", Monitor);
+                commandUpdate.Parameters.AddWithValue("$isOrderConfirmed", ConfirmOrder);
+
                 int nRows = commandUpdate.ExecuteNonQuery();
-                if(nRows == 0){
+                if(nRows == 0)
+                {
                     var commandInsert = connection.CreateCommand();
                     commandInsert.CommandText =
                     @"
-            INSERT INTO orders(size, phone)
-            VALUES($size, $phone)
-        ";
-                    // commandInsert.Parameters.AddWithValue("$size", Size);
-                    // commandInsert.Parameters.AddWithValue("$phone", Phone);
+                    INSERT INTO pcOrders(tokenId, motherboard, processor, ram, storageSSD, graphicsCard, cpuCase, os, monitor, isOrderConfirmed)
+                    VALUES($_tokenNumber, $motherboard, $processor, $ram, $storageSSD, $graphicsCard, $cpuCase, $os, $monitor, $isOrderConfirmed)";
+                    commandInsert.Parameters.AddWithValue("$motherboard", Motherboard);
+                    commandInsert.Parameters.AddWithValue("$processor", Processor);
+                    commandInsert.Parameters.AddWithValue("$ram", RAM);
+                    commandInsert.Parameters.AddWithValue("$storageSSD", Storage);
+                    commandInsert.Parameters.AddWithValue("$graphicsCard", Graphics);
+                    commandInsert.Parameters.AddWithValue("$cpuCase", CPU_Case);
+                    commandInsert.Parameters.AddWithValue("$os", OS);
+                    commandInsert.Parameters.AddWithValue("$monitor", Monitor);
+                    commandInsert.Parameters.AddWithValue("$isOrderConfirmed", ConfirmOrder);
+                    commandInsert.Parameters.AddWithValue("$_tokenNumber", TokenNumber);
                     int nRowsInserted = commandInsert.ExecuteNonQuery();
-
                 }
             }
 
--- OrderBot/Session.cs
+++ OrderBot/Session.cs
@@ -6,16 +6,16 @@ namespace OrderBot
     {
         private enum State
         {
-            WELCOMING, MOTHERBOARD, PROCESSOR, RAM, STORAGE_SSD, GRAPHICS_CARD, CPU_CASE, OS, MONITOR
+            WELCOMING, MOTHERBOARD, PROCESSOR, RAM, STORAGE_SSD, GRAPHICS_CARD, CPU_CASE, OS, MONITOR, CONFIRM_ORDER
         }
 
         private State nCur = State.WELCOMING;
         private Order oOrder;
 
-        public Session(string sPhone)
+        public Session(string sPhone)  
         {
             this.oOrder = new Order();
-            // this.oOrder.Phone = sPhone;
+            this.oOrder.TokenNumber = GenerateRandomTokenNumber();
         }
 
         public List<String> OnMessage(String sInMessage)
@@ -30,32 +30,97 @@ namespace OrderBot
                     break;
                 case State.MOTHERBOARD:
                     this.oOrder.Motherboard = sInMessage;
-                    // this.oOrder.Save();
+                    this.oOrder.Save();
                     aMessages.Add("Now, which processor would you like to install in your PC? Be Specific.  " /*+ this.oOrder.Size + " Shawarama?"*/);
                     this.nCur = State.PROCESSOR;
                     break;
                 case State.PROCESSOR:
                     this.oOrder.Processor = sInMessage;
+                    this.oOrder.Save();
                     aMessages.Add("Now, which RAM would you like to install into this desktop and what is size of the RAM you would like " /*+ this.oOrder.Size + " " + sProtein + " Shawarama?"*/);
                     this.nCur = State.RAM;
                     break;
                 case State.RAM:
                     this.oOrder.RAM = sInMessage;
+                    this.oOrder.Save();
                     aMessages.Add("Which storage SSD would you like to fit into your PC? Mention the size as well.");
                     this.nCur = State.STORAGE_SSD;
                     break;
                 case State.STORAGE_SSD:
                     this.oOrder.Storage = sInMessage;
+                    this.oOrder.Save();
                     aMessages.Add("Now which graphics card would you like to install in your desktop? Mention the graphics card size as well.");
                     this.nCur = State.GRAPHICS_CARD;
                     break;
-
+                case State.GRAPHICS_CARD:
+                    this.oOrder.Graphics = sInMessage;
+                    this.oOrder.Save();
+                    aMessages.Add("Which CPU Case would you like to select for your desktop build?");
+                    this.nCur = State.CPU_CASE;
+                    break;
+                case State.CPU_CASE:
+                    this.oOrder.CPU_Case = sInMessage;
+                    this.oOrder.Save();
+                    aMessages.Add("Which OS would you like to install in your desktop?");
+                    this.nCur = State.OS;
+                    break;
+                case State.OS:
+                    this.oOrder.OS = sInMessage;
+                    this.oOrder.Save();
+                    aMessages.Add("Which monitor would you like to accompany with your cpu? Mention in details.");
+                    this.nCur = State.MONITOR;
+                    break;
+                case State.MONITOR:
+                    this.oOrder.Monitor = sInMessage;
+                    this.oOrder.Save();
+                    aMessages.Add("Would you like to confirm your order? Type 'Y' or 'N' as your response.");
+                    this.nCur = State.CONFIRM_ORDER;    //next state is confirm order state
+                    break;
+                case State.CONFIRM_ORDER:
+                    this.oOrder.ConfirmOrder = sInMessage;
+                    this.oOrder.Save();
+                    if(this.oOrder.ConfirmOrder == "Y" || this.oOrder.ConfirmOrder == "y")
+                    {
+                        aMessages.Add("Your order is successfully confirmed!");
+                        aMessages.Add("Here are your custom desktop specifications:");
+                        aMessages.Add($"Token Number: {this.oOrder.TokenNumber}");
+                        aMessages.Add($"Motherboard: {this.oOrder.Motherboard}");
+                        aMessages.Add($"Processor: {this.oOrder.Processor}");
+                        aMessages.Add($"RAM: {this.oOrder.RAM}");
+                        aMessages.Add($"Storage SSD: {this.oOrder.Storage}");
+                        aMessages.Add($"Graphics Card: {this.oOrder.Graphics}");
+                        aMessages.Add($"CPU Case: {this.oOrder.CPU_Case}");
+                        aMessages.Add($"OS: {this.oOrder.OS}");
+                        aMessages.Add($"Monitor: {this.oOrder.Monitor}");
+                        aMessages.Add("Keep the token number safe and show it to the physical store and finish your payment transaction there.");
+                        aMessages.Add("Thank you for building a PC with us!");
+                    }
+                    else if(this.oOrder.ConfirmOrder == "N" || this.oOrder.ConfirmOrder == "n")
+                    {
+                        aMessages.Add("You have chosen not to confirm your order. Thank you for using our chat service.");
+                        aMessages.Add("Launch the chat service again, if you would like to build another desktop.");
+                    }
+                    //PLANNING ON ADDING A LOOP HERE SO WHEN THE CURRENT TRANSACTION IS DONE USER CAN RESET THE SESSION AND BUILD A PC AGAIN.
+                    // this.nCur = State.WELCOMING;    //set back to welcome state to order again
+                    break;
             }
             aMessages.ForEach(delegate (String sMessage)
             {
                 System.Diagnostics.Debug.WriteLine(sMessage);
             });
             return aMessages;
+        }
+
+        //THIS FUNCTION WILL GENERATE A RANDOM ALPHANUMERIC STRING WHICH WILL ACT AS OUR TOKEN NUMBER AND WILL BE CALLED WHEN A NEW SESSION IS CREATED.
+        public String GenerateRandomTokenNumber()
+        {
+            var allChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";  
+            var random = new Random();  
+            var resultToken = new string(  
+            Enumerable.Repeat(allChar , 8)  
+            .Select(token => token[random.Next(token.Length)]).ToArray());   
+            string authToken = resultToken.ToString();  
+            return authToken;
         }
 
     }
