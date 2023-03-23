e2f4d5bf55edaa35b2c7e1e1cc427a862fc2eb34
Preetb71 <preetbhatt.p@gmail.com>
2023-03-23 18:33:40 -0400

    Added PC Assembler Unit Tests

--- OrderBot.tests/OrderBotTest.cs
+++ OrderBot.tests/OrderBotTest.cs
@@ -17,7 +17,7 @@ namespace OrderBot.tests
                 var commandUpdate = connection.CreateCommand();
                 commandUpdate.CommandText =
                 @"
-        DELETE FROM orders
+        DELETE FROM pcOrders
     ";
                 commandUpdate.ExecuteNonQuery();
 
@@ -47,39 +47,39 @@ namespace OrderBot.tests
             Assert.True(nElapsed < 10000);
         }
         [Fact]
-        public void TestShawarama()
+        public void TestPCAssemblerIntro()
         {
             Session oSession = new Session("12345");
             String sInput = oSession.OnMessage("hello")[0];
-            Assert.True(sInput.ToLower().Contains("shawarama"));
+            Assert.True(sInput.ToLower().Contains("pc"));
+            Assert.True(sInput.ToLower().Contains("assemblers"));
         }
         [Fact]
-        public void TestSize()
+        public void TestMotherboard()
         {
             Session oSession = new Session("12345");
             String sInput = oSession.OnMessage("hello")[1];
-            Assert.True(sInput.ToLower().Contains("size"));
+            Assert.True(sInput.ToLower().Contains("motherboard"));
         }
         [Fact]
-        public void TestLarge()
+        public void TestProcessor()
         {
             Session oSession = new Session("12345");
             oSession.OnMessage("hello");
-            String sInput = oSession.OnMessage("large")[0];
-            Assert.True(sInput.ToLower().Contains("protein"));
-            Assert.True(sInput.ToLower().Contains("large"));
+            String sInput = oSession.OnMessage("Razor Motherboard")[0]; //OnMessage is passed as Razor Motherboard as an answer to the first question by the system, so that the next state is called and existence or processor can be tested.
+            Assert.True(sInput.ToLower().Contains("processor"));
         }
         [Fact]
-        public void TestChicken()
+        public void TestRAM()
         {
             string sPath = DB.GetConnectionString();
             Session oSession = new Session("12345");
             oSession.OnMessage("hello");
-            oSession.OnMessage("large");
-            String sInput = oSession.OnMessage("chicken")[0];
-            Assert.True(sInput.ToLower().Contains("toppings"));
-            Assert.True(sInput.ToLower().Contains("large"));
-            Assert.True(sInput.ToLower().Contains("chicken"));
+            oSession.OnMessage("Razor Motherboard");
+            String sInput = oSession.OnMessage("Intel I7 11th Gen Processor")[0];
+            Assert.True(sInput.ToLower().Contains("ram"));
         }
+
+        //Like so, existence of all the component states of the desktop can be tested by passing in user input using "OnMessage" function of the Session class.
     }
 }
